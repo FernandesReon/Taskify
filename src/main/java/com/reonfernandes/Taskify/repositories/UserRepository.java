5e76d4c3dc9069781986682640d01000654e2af2
Reon Fernandes <reondias@gmail.com>
2024-12-17 18:15:30 +0530

    User repository added.

--- src/main/java/com/reonfernandes/Taskify/repositories/UserRepository.java
+++ src/main/java/com/reonfernandes/Taskify/repositories/UserRepository.java
@@ -1,2 +1,9 @@
-package com.reonfernandes.Taskify.repositories;public interface UserRepository {
+package com.reonfernandes.Taskify.repositories;
+
+import com.reonfernandes.Taskify.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
